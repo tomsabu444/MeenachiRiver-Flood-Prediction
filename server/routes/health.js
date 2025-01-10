1ec16491b6761c8cf34da64440e39659bd1771ea
TOM SABU <tomsabu0000@gmail.com>
2025-01-10 14:07:55 +0530

    Update health check response message for MeenachiRiver Flood Prediction backend

--- server/routes/health.js
+++ server/routes/health.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send(' <h2> Communets-E-commerce Admin backend Working ✅ </h2>');
+    res.send(' <h2> MeenachiRiver Flood Prediction backend Working ✅ </h2>');
 });
 
 module.exports = router;
