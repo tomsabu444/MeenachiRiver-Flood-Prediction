9ff13fe4d1630dbc9da4cba71204783fdd103a87
TOM SABU <tomsabu0000@gmail.com>
2025-01-17 01:04:37 +0530

    Add node-cron for hourly data fetching and implement data storage logic

--- server/server.js
+++ server/server.js
@@ -3,7 +3,8 @@ const cros = require("cors");
 const connectDB = require("./config/databaseConnect");
 const NodeAuthenticate = require("./middleware/NodeAuthenticate");
 require("dotenv").config();
-
+const fetchAndStoreData = require(".//utils/hourlyfetch_kidangoorData");
+const cron = require("node-cron");
 
 const app = express();
 const port = process.env.PORT;
@@ -18,6 +19,14 @@ app.use("/", require("./routes/health")); //? Test route to check if the server 
 app.use("/v1/node-data/add",NodeAuthenticate, require("./routes/nodeEndpoint")); //? Route to receive data from IoT nodes
 app.use("/v1/node-metadata", require("./routes/nodeMetadataRoutes")); //? Route to get node metadata
 app.use("/v1/water-level", require("./routes/getWaterlevel")); //? Route to get water level data
+
+// Schedule the hourly task
+cron.schedule("0 * * * *", async () => {
+  console.log(`[${new Date().toISOString()}] Running hourly data fetch cycle...`);
+  await fetchAndStoreData();
+});
+// Run immediately on start
+fetchAndStoreData();
 
 app.listen(port, () =>
     console.log(`Server is running on port  http://localhost:${port}`)
