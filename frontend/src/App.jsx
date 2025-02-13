81de0db8dfa4dd68b6364d7d23741167fdbb2d2a
TOM SABU <tomsabu0000@gmail.com>
2025-02-14 00:22:58 +0530

    Add AutoPopupAlert component for automatic alert notifications

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,10 +6,12 @@ import Navbar from "./components/navbar";
 import Layout from "./components/layout";
 import Detail from "./components/Detail";
 import AlertPage from "./page/AlertPage";
+import AutoPopupAlert from "./components/AutoPopupAlert";
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
+      <AutoPopupAlert />
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route path="/" element={<HomePage />} />
